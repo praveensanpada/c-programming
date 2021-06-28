acd397bdfdd8b782099599cec96c3425454f5823
Praveen Sanpada <60234841+praveensanpada@users.noreply.github.com>
2021-06-28 17:47:11 +0530

    Create call-by-value.c

---
+++ call-by-value.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+//simple
+void swap(int, int);
+
+int main(){
+	int a=4,b=5;
+	printf("Before\n");
+	printf("%d %d\n",a,b);
+	swap(a,b);
+	printf("After\n");
+	printf("%d %d",a,b);
+	return 0;
+}
+
+void swap(int a, int b){
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
